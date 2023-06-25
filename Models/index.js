3ad4f044a2af56c63e059445791f079d73b9625b
chicojnr <souza.jnr@gmail.com>
2023-06-25 03:59:25 -0400

    fix 0.0.8

--- Models/index.js
+++ Models/index.js
@@ -5,17 +5,11 @@ const { Sequelize, DataTypes } = require('sequelize')
 //port for my database is 5433
 //database name is discover
 //const sequelize = new Sequelize(`oregon-postgres.render.com`, { dialect: "postgres" })
-const sequelize = new Sequelize('chatreplay', 'chatreplay_user', 'g8xuYcnUluVpPwb7yTOGxHRAhb0SJtgT',
-    { 
-        host: 'postgres://dpg-ci9i2gh8g3ne2ejvdct0-a/chatreplay',
-        dialect: "postgres" 
-    });
-
-    //postgres://chatreplay_user:g8xuYcnUluVpPwb7yTOGxHRAhb0SJtgT@dpg-ci9i2gh8g3ne2ejvdct0-a/chatreplay
+const sequelize = new Sequelize(`postgres://chatreplay_user:g8xuYcnUluVpPwb7yTOGxHRAhb0SJtgT@oregon-postgres.render.com/chatreplay?ssl=true`, { dialect: "postgres" })
 
 //checking if connection is done
 sequelize.authenticate().then(() => {
-    console.log(`Database connected to chtreplay`)
+    console.log(`Database connected to discover`)
 }).catch((err) => {
     console.log(err)
 })
